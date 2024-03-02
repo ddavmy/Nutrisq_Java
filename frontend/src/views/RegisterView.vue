e8bd9d676e048f7d35ba8fb26d1c86599785e628
ddavmy <ddavmy@gmail.com>
2024-03-02 02:14:27 +0100

    Rejestracja - wymagane poprawki

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,59 +1,77 @@
 <template>
-  
-<Header />
+  <Header />
 
-<!-- Main -->
-<div id="main">
+  <!-- Main -->
+  <div id="main">
 
-<!-- Portfolio -->
+    <!-- Portfolio -->
     <section id="portfolio" class="two">
-        <div class="container">
-          <form>
-            <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="inputEmail">Email</label>
-                <input type="email" class="form-control" id="inputEmail">
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputPassword">Password</label>
-                <input type="password" class="form-control" id="inputPassword">
-              </div>
+      <div class="container">
+        <form @submit.prevent="saveData">
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail">Email</label>
+              <input v-model="user.email" type="email" class="form-control" id="inputEmail">
             </div>
-            <div class="form-group">
-              <label for="firstName">First Name</label>
-              <input type="text" class="form-control" id="firstName">
+            <div class="form-group col-md-6">
+              <label for="inputPassword">Password</label>
+              <input v-model="user.password" type="password" class="form-control" id="inputPassword">
             </div>
-            <div class="form-group">
-              <label for="lastName">Last Name</label>
-              <input type="text" class="form-control" id="lastName">
-            </div>
-            <button type="submit" class="btn btn-primary">Sign Up</button>
-          </form>
-        </div>
+          </div>
+          <div class="form-group">
+            <label for="firstName">First Name</label>
+            <input v-model="user.name" type="text" class="form-control" id="firstName">
+          </div>
+          <div class="form-group">
+            <label for="lastName">Last Name</label>
+            <input v-model="user.lastName" type="text" class="form-control" id="lastName">
+          </div>
+          <button type="submit" class="btn btn-primary">Sign Up</button>
+        </form>
+      </div>
     </section>
 
-</div>
+  </div>
 
-<Footer />
+  <Footer />
 
 </template>
 
 <script>
 import Header from './pages/Header.vue';
 import Footer from './pages/Footer.vue';
+import axios from 'axios';
 
 export default {
   components: {
     Header,
     Footer
   },
-  name: 'RegisterView'
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+        name: '',
+        lastName: '',
+        roleId: 1 // Set a default value for roleId
+      }
+    };
+  },
+  methods: {
+    saveData() {
+      axios.post("http://localhost:8080/api/user", this.user)
+      .then(response => {
+        console.log("User registered successfully:", response.data);
+        alert("User registered successfully");
+        // Optionally, you can navigate to another page or show a success message
+      })
+      .catch(error => {
+        console.error('Error registering user:', error.response.data);
+        alert("Error registering user");
+        // Optionally, you can display an error message to the user
+      });
+    }
+  }
 };
 </script>
-
-<style>
-#meals {
-  color: black;
-  text-align: center;
-}
-</style>
